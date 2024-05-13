86aabad3dee8c07f4cc55df17c77d3af721f6910
Yduan Rech Effting <105087789+yduanrech@users.noreply.github.com>
2024-05-13 18:38:44 -0300

    validacao pessoas ok

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var updateProgressBar;
 
 function displayStep(stepNumber) {
     if (stepNumber >= 1 && stepNumber <= 4) {
+        if (!validateStep(currentStep)) return; // Adiciona validação aqui
         $(".step-" + currentStep).hide();
         $(".step-" + stepNumber).show();
         currentStep = stepNumber;
@@ -10,21 +11,35 @@ function displayStep(stepNumber) {
     }
 }
 
+function validateStep(step) {
+  var $step = $(`.step-${currentStep}`);
+  var isValid = true;
+  $step.find('input:required, select:required').each(function() {
+    if (!this.checkValidity()) {
+      $(this).addClass('is-invalid');
+      isValid = false;
+    } else {
+      $(this).removeClass('is-invalid');
+    }
+  });
+  return isValid;
+}
+
 function calcularConsumo() {
     var adultos = parseInt($('#adultosQuantidade').val());
     var criancas = parseInt($('#criancasQuantidade').val());
     var duracao = parseInt($('#duracaoHoras').val());
-    
+
     var selecoesCarnes = $('.step-3 .form-check-input:checked').map(function() {
       return $(this).val();
     }).get();
-    
+
     var selecoesBebidas = $('.step-4 .form-check-input:checked').map(function() {
       return $(this).val();
     }).get();
-    
+
     var quantCarnes = selecoesCarnes.length;
-    
+
     var carnePorAdulto, carnePorCrianca;
     if (duracao === 1) {  // Menos de 2 horas
       carnePorAdulto = 500;
@@ -36,10 +51,10 @@ function calcularConsumo() {
       carnePorAdulto = 850;
       carnePorCrianca = 350;
     }
-    
+
     var carnePorTipoAdulto = carnePorAdulto / quantCarnes;
     var carnePorTipoCrianca = carnePorCrianca / quantCarnes;
-  
+
     var resultadoCarnes = {};
     selecoesCarnes.forEach(function(tipo) {
       resultadoCarnes[tipo] = {
@@ -47,26 +62,25 @@ function calcularConsumo() {
         'criancas': parseFloat(carnePorTipoCrianca.toFixed(0)) * criancas + 'g'
       };
     });
-  
-    // Bebidas
+
     var totalCerveja = selecoesBebidas.includes("Cerveja") ? 6 * adultos + ' latas' : "0 latas";
     var totalRefri = selecoesBebidas.includes("Refrigerante") ? (3 * adultos + criancas) + ' latas' : "0 latas";
     var totalVodka = selecoesBebidas.includes("Vodka") ? adultos + ' garrafas' : "0 garrafas";
-  
-    // Salva os resultados no localStorage
+
     localStorage.setItem('resultadoCarnes', JSON.stringify(resultadoCarnes));
     localStorage.setItem('totalCerveja', totalCerveja);
     localStorage.setItem('totalRefri', totalRefri);
     localStorage.setItem('totalVodka', totalVodka);
-  
+
     console.log('Cálculo realizado com sucesso!');
 }
 
 $(document).ready(function() {
     $('#multi-step-form').find('.step').slice(1).hide();
-  
+
     $(".next-step").click(function() {
         if (currentStep < 4) {
+            if (!validateStep(currentStep)) return;  // Verifica a validade antes de ir para o próximo passo
             $(".step-" + currentStep).addClass("animate__animated animate__fadeOutLeft");
             currentStep++;
             setTimeout(function() {
@@ -79,6 +93,7 @@ $(document).ready(function() {
 
     $(".prev-step").click(function() {
         if (currentStep > 1) {
+            if (!validateStep(currentStep)) return;  // Verifica a validade antes de voltar para o passo anterior
             $(".step-" + currentStep).addClass("animate__animated animate__fadeOutRight");
             currentStep--;
             setTimeout(function() {
@@ -96,7 +111,8 @@ $(document).ready(function() {
 
     $('#multi-step-form').on('submit', function(event) {
         event.preventDefault(); // Evita o envio do formulário
-        calcularConsumo(); // Chama a função para calcular o consumo
-        window.location.href = 'resultado.html'; // Redireciona para outra página, resultado.html
+        if (!validateStep(currentStep)) return; // Verifica o passo atual antes de enviar
+        calcularConsumo();
+        window.location.href = 'resultado.html';
     });
 });
